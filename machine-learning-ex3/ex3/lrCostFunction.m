d577b232297052eeaa1e2ec5a84d8256ac8f2394
sunil <sunilssaharan@gmail.com>
2017-12-05 18:04:34 +0530

    made cost function and gradient for one vs alll logistic regression

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,7 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
+J = (1/m)*(-1*y'*log(sigmoid(X*theta))-(1-y')*(log(1-sigmoid(X*theta)))) +...
+ (lambda/(2*m))*sum(theta.*theta) - (lambda/(2*m))*(theta(1)*theta(1));
+grad =(1/m)*((sigmoid(X*theta)-y)'*X)'.+((lambda/m)*theta);
+grad(1) = grad(1) -(lambda/m)*theta(1);
 
 
 
